96c611c3a46a7ddf7cbe741d802a95249a349bfa
MarkCherepovskyi <mark.cherepovskyi@gmail.com>
2026-03-10 15:27:06 +0200

    prestart task

--- internal/config/tasks.go
+++ internal/config/tasks.go
@@ -25,6 +25,7 @@ type EventsConfiger interface {
 type EventConfig struct {
 	Event    string   `fig:"event,required"`
 	TaskType TaskType `fig:"task_type,required"`
+	PreStart bool     `fig:"prestart"`
 }
 
 // eventsConfigRaw is used for parsing the config file
@@ -46,11 +47,11 @@ type eventsConfig struct {
 func (c *eventsConfig) EventsConfig() []EventConfig {
 	return c.once.Do(func() interface{} {
 		raw := kv.MustGetStringMap(c.getter, eventsConfigKey)
-		config := new(eventsConfigRaw)
-		err := figure.Out(config).With(figure.BaseHooks).From(raw).Please()
+		сfg := new(eventsConfigRaw)
+		err := figure.Out(сfg).With(figure.BaseHooks).From(raw).Please()
 		if err != nil {
 			panic(errors.Wrap(err, "failed to figure out events config"))
 		}
-		return config.List
+		return сfg.List
 	}).([]EventConfig)
 }
